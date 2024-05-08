79f9f09ed92f9b200c5325edfe977b17c59762ab
riccardotorre <riccardo.torre@gmail.com>
2024-05-08 09:04:54 +0200

    reorganized code

--- GMetrics/__init__.py
+++ GMetrics/__init__.py
@@ -8,3 +8,5 @@ from .metrics.swd_metrics import SWDMetric
 from .base import TwoSampleTestBase
 from .base import TwoSampleTestInputs
 from .base import TwoSampleTestResult
+from . import metrics
+from . import utils
